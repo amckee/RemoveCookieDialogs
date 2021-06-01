dcaec470e2c6e285a962fee8cb8d94dca948afce
Adam McKee <armware+dev@gmail.com>
2021-06-01 17:09:12 -0500

    first upload

---
+++ removecookiedialogs.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name        Remove Cookie Agreement Boxes
+// @version     2021.06.01.1
+// @namespace   https://github.com/amckee/RemoveCookieDialogs
+// @description Removes the super annoying 'we use cookies' boxes, at least as many as I can.
+// @author      Adam McKee
+// @include     http*://stackoverflow.com/*
+// @updateURL   
+// @supportURL  https://github.com/amckee/RemoveCookieDialogs/issues
+// @run-at      document-end
+// ==/UserScript==
+
+setTimeout(function() {
+    // videos
+    var el = document.querySelector("body > div.ff-sans.ps-fixed.z-nav-fixed.ws4.sm\\:w-auto.p32.sm\\:p16.bg-black-750.fc-white.bar-lg.b16.l16.r16.js-consent-banner");
+    if( el != null ){
+        console.log("removing cache agreement box");
+        el.remove();
+    } else {
+        console.log("no dialog found");
+    }
+}, 1000);
